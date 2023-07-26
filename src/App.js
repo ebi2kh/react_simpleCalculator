d97a12e858b8f6a48776821195545216d0a169fe
ebrahim <eb.kh1372@gmil.com>
2023-07-27 01:00:05 +0330

    try to add theme

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React, { useState } from 'react'
 import './App.css'
 
+const themes = {
+  default: {
+    backgroundColor: '#f0f0f0',
+    buttonBgColor: '#e0e0e0',
+    buttonTextColor: '#333',
+  },
+  purple: {
+    backgroundColor: '#6c5ce7',
+    buttonBgColor: '#4b39a5',
+    buttonTextColor: '#fff',
+  },
+  green: {
+    backgroundColor: '#00b894',
+    buttonBgColor: '#009e74',
+    buttonTextColor: '#fff',
+  },
+}
+
 function App() {
   const [inputValue, setInputValue] = useState('')
+  const [selectedTheme, setSelectedTheme] = useState('default')
 
   const handleButtonClick = (buttonValue) => {
     if (buttonValue === 'C') {
@@ -30,8 +49,25 @@ function App() {
     setInputValue(inputValue + buttonValue)
   }
 
+  const themeStyles = themes[selectedTheme]
+
   return (
-    <div className='calculator'>
+    <div
+      className='calculator'
+      style={{ backgroundColor: themeStyles.backgroundColor }}
+    >
+      {/* <div className='theme-selector'> */}
+      {/* <label htmlFor='themeSelect'>Choose a Theme:</label>
+      <select
+        id='themeSelect'
+        value={selectedTheme}
+        onChange={(e) => setSelectedTheme(e.target.value)}
+      >
+        <option value='default'>Default</option>
+        <option value='purple'>Purple</option>
+        <option value='green'>Green</option>
+      </select> */}
+      {/* </div> */}
       <input type='text' id='result' value={inputValue} readOnly />
       <div className='buttons'>
         <button onClick={() => handleButtonClick('C')} className='clear'>
